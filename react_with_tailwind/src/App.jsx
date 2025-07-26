680468c9c52ff20cdaff207e69e828ca612dea3f
Emovils <oviliekenelucky@gmail.com>
2025-07-26 14:35:34 -0700

    Merged remote changes

---
+++ react_with_tailwind/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+         <p className="bg-blue-500">Hello World</p>
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
