224140ad98202f095fb50739be9cb7d649704444
Emovils <oviliekenelucky@gmail.com>
2025-07-31 03:35:45 -0700

    src/App.jsx

--- react_with_tailwind/src/App.jsx
+++ react_with_tailwind/src/App.jsx
@@ -5,13 +5,23 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
       <div>
-         <p className="bg-blue-500">Hello World</p>
+        
+
+         <h1 className="text-purple-700 font-bold text-3xl">
+          Ovili Ekene
+         </h1>
+         <h2 className="text-green-600 font-semibold text-xl">
+          Software Developer
+         </h2>
+         <p className="text-gray-700 font-medium"> 
+          i am a sofware developer(in training) who enjoys exploring and building web interfaces
+          i am currently learning about tailwind, and when am not coding am creating amazing outfits or helping busy CEO's
+         </p>
         
       </div>
       
-    </>
+   
   )
 }
 
