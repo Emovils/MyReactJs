c12eb8f2ac31edc817e599e77eb1a588c442da60
Emovils <oviliekenelucky@gmail.com>
2025-08-05 02:00:46 -0700

    src/App.jsx

--- react_with_tailwind/src/App.jsx
+++ react_with_tailwind/src/App.jsx
@@ -5,20 +5,26 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div>
+    <div className="bg-gray-100 p-10 m-4 border border-gray-400 rounded-lg">
+      <div className="flex items-center justify-between bg-white p-4 mb-6 border-b border-gray-300">
         
 
-         <h1 className="text-purple-700 font-bold text-3xl">
+         <h1 className="text-purple-700 font-bold text-3xl capatilize text-center">
           Ovili Ekene
+          
          </h1>
          <h2 className="text-green-600 font-semibold text-xl">
           Software Developer
          </h2>
-         <p className="text-gray-700 font-medium"> 
+         
+
+         <p className="text-gray-700 font-medium font-mono text-justify"> 
           i am a sofware developer(in training) who enjoys exploring and building web interfaces
           i am currently learning about tailwind, and when am not coding am creating amazing outfits or helping busy CEO's
          </p>
+         
         
+      </div>
       </div>
       
    
